b019ad812b49fc4e45facd4cabd65518fcc841aa
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-14 00:56:38 +0500

    Typo fixes

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -6,8 +6,8 @@ export const LittleLemonHeader = () => {
     const _str = strings.en;
 
     return (
-    <View style={{flex:0.2,justifyContent:'center',alignItems:'center', backgroundColor: `${palette.yellow}` }}>
-        <Text style={{ fontSize:30,  color: `${palette.black}` }}>{_str.little_lemon_header}</Text>
+    <View style={{justifyContent:'center',alignItems:'center', backgroundColor: `${palette.yellow}` }}>
+        <Text style={{padding:16, fontSize:30,  color: `${palette.black}` }}>{_str.little_lemon_header}</Text>
     </View>
   )
 }

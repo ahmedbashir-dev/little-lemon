35315fc5ee69419ab62fe45bab1f1a095afa4e96
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-14 00:54:04 +0500

    Welcome message string added

--- utils/constants.js
+++ utils/constants.js
@@ -2,6 +2,7 @@ const currentYear = new Date().getFullYear();
 export default strings = {
     en:{
         little_lemon_header:'Little Lemon',
-        little_lemon_footer:`All Rights Reserved by Little Lemon, ${currentYear}`
+        little_lemon_footer:`All Rights Reserved by Little Lemon, ${currentYear}`,
+        little_lemon_welcome:'Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!'
     }
 }

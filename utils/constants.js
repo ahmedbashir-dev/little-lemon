0f29b932c36baa9314b762bbee16516bea8e9a4d
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-13 22:28:38 +0500

    string resources added

---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+export default strings = {
+    en:{
+        little_lemon_header:'Little Lemon',
+        little_lemon_footer:`All Rights Reserved by Little Lemon, ${currentYear}`
+    }
+}

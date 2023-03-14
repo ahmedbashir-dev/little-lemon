75b5a10a9110f4299e136e691d2541866c142db6
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-15 02:49:18 +0500

    Footer stylesheet added

--- components/LittleLemonFooter.js
+++ components/LittleLemonFooter.js
@@ -1,11 +1,21 @@
-import { View , Text} from "react-native"
+import { View, Text, StyleSheet } from "react-native";
 import strings from "../utils/constants";
 import palette from "../utils/palette";
 export const LittleLemonFooter = () => {
-    const _str = strings.en;
-    return (
-    <View style={{backgroundColor:`${palette.yellow}`, alignItems:'center'}}>
-        <Text style={{padding:4}}>{_str.little_lemon_footer}</Text>
-    </View>
-  )
-}
+	const _str = strings.en;
+	return (
+		<View style={styles.container}>
+			<Text style={styles.footerText}>{_str.little_lemon_footer}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: `${palette.yellow}`,
+		alignItems: "center",
+	},
+  footerText:{
+    padding: 4 
+  }
+});

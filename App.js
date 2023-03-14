880ba9d70038e77a240991fdd34864267a2303c6
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-15 02:47:09 +0500

    App stylesheet added

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { LittleLemonFooter } from "./components/LittleLemonFooter";
 import { LittleLemonHeader } from "./components/LittleLemonHeader";
 import { LittleLemonWelcome } from "./components/LittleLemonWelcome";
 import palette from "./utils/palette";
 export default function App() {
-  return (
-    <View style={{
-      flex:1,
-      backgroundColor:`${palette.green}`,
-      justifyContent:'space-between',
-
-    }}>
-      <LittleLemonHeader />
-      <LittleLemonWelcome />
-      <LittleLemonFooter />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<LittleLemonHeader />
+			<LittleLemonWelcome />
+			<LittleLemonFooter />
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: `${palette.green}`,
+		justifyContent: "space-between",
+	},
+});

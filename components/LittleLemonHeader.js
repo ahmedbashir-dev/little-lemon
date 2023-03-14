2e9be1f753b4adeba1b6a044428eee26d0c4ccd9
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-15 02:34:55 +0500

    StyleSheet API incorporated in Header

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import strings from "../utils/constants";
-import palette from '../utils/palette';
+import palette from "../utils/palette";
 export const LittleLemonHeader = () => {
-    const _str = strings.en;
+	const _str = strings.en;
 
-    return (
-    <View style={{justifyContent:'center',alignItems:'center', backgroundColor: `${palette.yellow}` }}>
-        <Text style={{padding:16, fontSize:30,  color: `${palette.black}` }}>{_str.little_lemon_header}</Text>
-    </View>
-  )
-}
+	return (
+		<View style={headerStyles.container}>
+			<Text style={headerStyles.headerText}>{_str.little_lemon_header}</Text>
+		</View>
+	);
+};
+
+const headerStyles = StyleSheet.create({
+	container: {
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: `${palette.yellow}`,
+	},
+	headerText: {
+		padding: 16,
+		fontSize: 30,
+		color: `${palette.black}`,
+	},
+});
